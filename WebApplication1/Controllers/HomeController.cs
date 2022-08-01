d609dd868d6b32b7166875989580016d637f75f8
NarasimhaRaoSomarouthu <102780781+NarasimhaRaoSomarouhtu@users.noreply.github.com>
2022-08-01 17:32:07 +0530

    first commit

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ServiceModel;
+using WebApplication1.ServiceReference1;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        HelloServiceClient client = new HelloServiceClient();
+        public ActionResult Index()
+        {
+            ViewBag.Message = client.getMessage();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
